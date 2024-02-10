5b3379ad82f0f7de293343792dd088097a2d5a2e
Piccolo1119 <julio.vazquez.1bach@gmail.com>
2024-02-10 12:57:31 +0100

    arreglos en develop nº1

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -10,3 +10,34 @@ const arrows = document.querySelectorAll('.arrow');
 
       });
     });
+    var cameraStream;
+
+    function openCamara() {
+        //Solicitar permisos para acceder a la camara
+        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({ video: true}, function (stream) {
+                //mostrar el contendeor de la camara y establecer el flujo en el
+
+                document.getElementById("contenedor").style.display = "block";
+                document.getElementById('camaraFeed').srcObject = stream;
+                cameraStream = stream;
+            }, function(error) {
+                console.log('No se pudo obtener acceso a la cámara: ', error);
+            });
+        } else {
+            console.error('getUserMedia is not supported in this browser')
+        }
+    }
+
+    function closeCamara() {
+        //detener la transmision de la camara y ocultar el contenedor
+        if (cameraStream) {
+            cameraStream.getTracks().forEach(function (track){
+                track.stop();
+                
+            });
+            document.getElementById("contenedor").style.display = "none";
+        }
+    }
