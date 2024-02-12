95a395f9901605452d449025721babc402520aaf
Piccolo1119 <julio.vazquez.1bach@gmail.com>
2024-02-12 14:44:35 +0100

    arreglos en develop nº2. ahora hay que poner los objetos window bien

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,5 +1,4 @@
 const arrows = document.querySelectorAll('.arrow');
-    console.log(arrows);
     // Itera sobre cada flecha y agrega un evento de clic
     arrows.forEach(arrow => {
       arrow.addEventListener('click', function () {
@@ -10,6 +9,9 @@ const arrows = document.querySelectorAll('.arrow');
 
       });
     });
+
+
+    //camara abrir, cerrar
     var cameraStream;
 
     function openCamara() {
@@ -41,3 +43,65 @@ const arrows = document.querySelectorAll('.arrow');
             document.getElementById("contenedor").style.display = "none";
         }
     }
+
+
+    //ejercicio recordar cita dentista
+
+    const relojitos = document.querySelectorAll('.arrow.reloj');
+
+    // Itera sobre cada elemento y agrega un event listener
+    relojitos.forEach(relojito => {
+        relojito.addEventListener('click', () => {
+            let identificador = window.setInterval(() => {
+                let respuesta = window.confirm("Tienes cita con el dentista mañana a las 12.00. ¿Te los sigo recordando?");
+                if (respuesta === false) {
+                    window.clearInterval(identificador);
+                    console.log("Programa finalizado");
+                } else {
+                    console.log("Te lo sigo recordando");
+                }
+            });
+        });
+    });
+    
+
+    //info de pantalla 
+
+    const infoPantallita = document.querySelectorAll('.arrow.pantalla');
+
+    infoPantallita.forEach(pantalla => {
+        pantalla.addEventListener('click', () => {
+            let infoPantalla = "availHeight: " + screen.availHeight + "<br><br>";
+            infoPantalla += "availWidth: " + screen.availWidth + "<br><br>";
+            infoPantalla += "height: " + screen.height + "<br><br>";
+            infoPantalla += "width: " + screen.width + "<br><br>";
+            infoPantalla += "colorDepth:" +screen.colorDepth + "<br><br>";
+            infoPantalla += "pixelDepth: " + screen.pixelDepth + "<br><br>";
+            infoPantalla += "orientation: " + screen.orientation + "<br><br>";
+            infoPantalla += "orientation: " + screen.orientation.type + "<br><br>";
+
+            const contentElement = document.querySelector('.content');
+            contentElement.innerHTML =  '<h2>Información de pantalla</h2>' +'<div class="pantalla-info">'+ infoPantalla + '</div>';
+        })
+    })
+
+
+    const infoNavegador = document.querySelectorAll('.arrow.navegador');
+
+    infoNavegador.forEach(pantalla => {
+        pantalla.addEventListener('click', () => {
+            let infoNavegador = "clipboard: " + navigator.clipboard + "<br>";
+            infoNavegador += "cookieEnabled: " + navigator.cookiEnabled+ "<br>";
+            infoNavegador += "geolocation: " + navigator.geolocation + "<br>";
+            infoNavegador += "language: " + navigator.language + "<br>";
+            infoNavegador += `onLine: ${navigator.onLine}`;
+            infoNavegador += "plugins: " + navigator.plugins+"<br>";
+            infoNavegador += "storage: " + navigator.storage+"<br>";
+            infoNavegador += "userAgent: " + navigator.userAgent+"<br>";
+            console.log(infoNavegador);
+
+            const contentElement = document.querySelector('.content');
+            contentElement.innerHTML =  '<h2>Información de pantalla</h2>' +'<div class="pantalla-info">'+ infoNavegador + '</div>';
+        })
+    })
+    
